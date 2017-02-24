1b47ee5aaa8a7a2c0d4406d74a706791e47497b0
“Richard <richgurney1987@gmail.com>
2017-02-24 07:43:29 +0000

    weather list

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
     console.log(temps);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color='red' />
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressures} color="blue" /></td>
+        <td><Chart data={humidities} color="red" /></td>
       </tr>
     )
   }
